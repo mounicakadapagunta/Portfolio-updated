f5246f04a68032801af15c68e1846f4926a3b662
mounicakadaagunta <mounicakadapagunta13@gmail.com>
2022-01-29 18:40:06 +0530

    html page updated

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 const navToogle = document.getElementById('nav-toogle');
 const navMenu = document.getElementById('nav-menu');
 const navClose = document.getElementById('nav-close');
-navToogle.addEventListener('click',()=>{
+navToogle.addEventListener('click', () => {
     navMenu.classList.add('show-menu');
-}) 
-navClose.addEventListener('click',()=>{
+})
+navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu')
 })
 const navItems = document.querySelectorAll('.nav__item');
 navItems.forEach(element => {
-    element.addEventListener('click',linkActive)
+    element.addEventListener('click', linkActive)
 });
-function linkActive(){
+function linkActive() {
     navMenu.classList.remove('show-menu');
 }
 // theme change
 const themeChange = document.getElementById('theme-change');
-if(localStorage.getItem('theme')===null){
-    localStorage.setItem('theme','light');
+if (localStorage.getItem('theme') === null) {
+    localStorage.setItem('theme', 'light');
 }
-if(localStorage.getItem('theme')==='light'){
+if (localStorage.getItem('theme') === 'light') {
     document.body.classList.add('body-dark-theme');
-        themeChange.classList.remove('uil-moon');
-        themeChange.classList.add('uil-sun');
+    themeChange.classList.remove('uil-moon');
+    themeChange.classList.add('uil-sun');
 }
-else if (localStorage.getItem('theme')==='dark'){
+else if (localStorage.getItem('theme') === 'dark') {
     document.body.classList.remove('body-dark-theme');
-        themeChange.classList.remove('uil-sun');
-        themeChange.classList.add('uil-moon');
+    themeChange.classList.remove('uil-sun');
+    themeChange.classList.add('uil-moon');
 }
-themeChange.addEventListener('click',()=>{
+themeChange.addEventListener('click', () => {
     let theme = localStorage.getItem('theme');
-    if(theme==='dark'){
+    if (theme === 'dark') {
         document.body.classList.add('body-dark-theme');
         themeChange.classList.remove('uil-moon');
         themeChange.classList.add('uil-sun');
-        localStorage.setItem('theme','light');
-    }else{
+        localStorage.setItem('theme', 'light');
+    } else {
         document.body.classList.remove('body-dark-theme');
         themeChange.classList.remove('uil-sun');
         themeChange.classList.add('uil-moon');
-        localStorage.setItem('theme','dark');
+        localStorage.setItem('theme', 'dark');
     }
 })
 
@@ -48,12 +48,12 @@ themeChange.addEventListener('click',()=>{
 // Skills Accordian 
 const skillsHeaders = document.querySelectorAll('.skills__header');
 const skillsContents = document.querySelectorAll('.skills__content');
-skillsContents.forEach((element,index) => {
-    element.addEventListener('click',()=>{
-        if(element.classList.contains('skills__open')){
+skillsContents.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        if (element.classList.contains('skills__open')) {
             element.classList.remove('skills__open');
-        }else{
-            skillsContents.forEach(e=>{
+        } else {
+            skillsContents.forEach(e => {
                 e.classList.remove('skills__open');
             })
             skillsContents[index].classList.add('skills__open');
@@ -64,36 +64,36 @@ skillsContents.forEach((element,index) => {
 
 
 // scrollTop 
-const scrollTopButton  = document.querySelector('.scrollTop__box');
-scrollTopButton.addEventListener('click',()=>{
+const scrollTopButton = document.querySelector('.scrollTop__box');
+scrollTopButton.addEventListener('click', () => {
     topFunction();
 })
-window.addEventListener('scroll',scrollFunction);
+window.addEventListener('scroll', scrollFunction);
 function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    scrollTopButton.style.display = "block";
-  } else {
-    scrollTopButton.style.display = "none";
-  }
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        scrollTopButton.style.display = "block";
+    } else {
+        scrollTopButton.style.display = "none";
+    }
 }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 // Injecting Dynamic Text
-let array = ['JavaScript','Frontend Developer','Backend Developer','Ui/Ux Developer','Programmer','Data Science','C','Python','Data Structures and Algorithms'];
+let array = ['MERN', 'Frontend Developer', 'Backend Developer'];
 const Inject = document.querySelectorAll('.inject');
 
-let i=0;
+let i = 0;
 setInterval(() => {
-    if(i<array.length){
-        Inject.forEach(e =>{
+    if (i < array.length) {
+        Inject.forEach(e => {
             e.innerText = array[i];
         })
         i++;
-    }else{
-        i=0;
+    } else {
+        i = 0;
     }
 }, 900);
